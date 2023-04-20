289edf5e45b6ebdf7f0af01461c1a74f5a288981
quoc <quochit879@gmail.com>
2023-04-20 23:02:43 +0700

    update code

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
+//variable global
 const query = 'limit=100&page=1';
-
 
 //api register
 function getDataFromInputRegister() {
@@ -29,6 +29,12 @@ async function callApiRegister(dataRegister) {
     localStorage.setItem("food-token", data.data.token);
     window.location.href = "/auth/login.html";
   } catch (e) {
+    let objOfToast = {
+      title: "error",
+      message: e.response.data.message,
+      duration: 3000
+    }
+    toastMessage(objOfToast)
     const register__error = document.querySelector(".register-error");
     register__error.innerHTML = e.response.data.message;
   }
@@ -55,13 +61,19 @@ async function callApiLogin(dataLogin) {
     );
     localStorage.setItem("food-token", data.data.data.token);
     window.location.href = "/index.html";
-
   } catch (e) {
+    console.log(e);
+    let objOfToast = {
+      title: "error",
+      message: e.response.data.message,
+      duration: 3000
+    }
+    toastMessage(objOfToast)
     const login__report = document.querySelector(".login-error");
     login__report.innerHTML = e.response.data.message;
-    console.log(e.response);
+    console.log(e);
   }
-} 
+}
 
 //api get user
 
@@ -74,9 +86,9 @@ async function getUser() {
     );
 
     return data.data;
-  
+
   } catch (e) {
-    // e.response.data.code === 401 && (window.location.href = "/auth/login.html");
+    e.response.data.code === 401 && (window.location.href = "/auth/login.html");
   }
 }
 
@@ -86,8 +98,8 @@ async function getCateList(cateLimit) {
   try {
     const data = await axios.get(`https://food-api.huytx.com/api/v1/cate/list?${cateLimit}`);
     return data.data;
-  }catch (e) {
-    console.log(e)    
+  } catch (e) {
+    console.log(e)
   }
 }
 
@@ -119,11 +131,11 @@ async function handleChangeCate() {
     tab.onclick = function () {
       const button = this.querySelector("button"); // Lấy button của mục đang được click
       const cateID = button.getAttribute("id"); // Lấy ID của mục đang được click
-        listFood(cateID);
+      listFood(cateID);
       tabs.forEach((tab) => {
         tab.classList.remove("active");
       });
-        this.classList.add("active");
+      this.classList.add("active");
     };
   })
 }
@@ -156,41 +168,52 @@ async function listFood(cateID, currentPage = 1) {
     const foodItem = document.createElement("div");
     foodItem.className = "menu-breakfast__1";
     foodItem.innerHTML = `
-        <img src="${food.images[0].imageUrl}" alt="HÌNH ẢNH" class="snack ">
-        <div class="menu-breakfast__icon">
-          <h6 class="name">${food.description}</h6>
-          <p class="price" href="">${food.foodName}</p>
-          <div class="sell-and-check">
+    <img src="${food.images[0].imageUrl}" alt="HÌNH ẢNH" class="snack ">
+    <div class="menu-breakfast__icon">
+        <h6 class="name">${food.description}</h6>
+        <p class="price" href="">${food.foodName}</p>
+        <div class="sell-and-check">
+    
             <span>${food.price}</span>
             <div class="icon-start">
-              <i class="fa-regular fa-star"></i>
-              <i class="fa-regular fa-star"></i>
-              <i class="fa-regular fa-star"></i>
-              <i class="fa-regular fa-star"></i>
-              <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
             </div>
-          </div>
-          <div class="link-cart">
-            <button id=${food.foodId} onclick="myfuction(event)" >ADD TO CART
-              <i class="fa-sharp fa-solid fa-right-long"></i>
+        </div>
+        <div id="buy-amount">
+            <button class="minus" onclick="handleMinusProduct(event)">
+                <i class="fa-solid fa-minus"></i>
+            </button>
+            <input type="text" class="amount" value="1" oninput="handleInput(event)">
+            <button class="plus" onclick="handlePlusProduct(event)">
+                <i class="fa-thin fa-plus"></i>
+            </button>
+        </div>
+        <div class="link-cart">
+            <button id=${food.foodId} onclick="addFoodToCart(event)">ADD TO CART
+                <i class="fa-sharp fa-solid fa-right-long"></i>
             </button>
             <div class="item-action">
-              <span class="icon-like icon-liked">
-                <i class="icon-like-empty fa-regular fa-heart"></i>
-                <i class="icon-like-fill fa-solid fa-heart"></i>
-              </span>
-              <div class="icon-eyes">
-                <i class="fa-solid fa-eye "></i>
-              </div>
+                <span class="icon-like icon-liked">
+                    <i class="icon-like-empty fa-regular fa-heart"></i>
+                    <i class="icon-like-fill fa-solid fa-heart"></i>
+                </span>
+                <div class="icon-eyes">
+                    <i class="fa-solid fa-eye "></i>
+                </div>
             </div>
-          </div>
+        </div>
+    </div>
         `;
     foodList.appendChild(foodItem);
   });
 
   // open pseudo 
-  
-// phân trang
+
+  // phân trang
   // Add pagination buttons
   const paginationContainer = document.querySelector(".pagination");
   paginationContainer.innerHTML = "";
@@ -213,20 +236,15 @@ async function listFood(cateID, currentPage = 1) {
   }
 }
 
-
-
-
-
-
 // phần của tuấn 
 
 async function nameCate() {
   const cates = await getCateList(cateLimit);
   const getNameFood = document.querySelector(".food-cate");
   await cates.data.map((cate, index) => {
-      const getName = document.createElement("div");
-      getName.className = "food-up";
-      getName.innerHTML = `
+    const getName = document.createElement("div");
+    getName.className = "food-up";
+    getName.innerHTML = `
             <img src="${cate.cateImage}" alt="HÌNH ẢNH" class="snack ">
                     <div class="food-item">
                         <span>
@@ -248,7 +266,7 @@ async function nameCate() {
                         </a>
                     </div>
       `;
-      getNameFood.appendChild(getName)
+    getNameFood.appendChild(getName)
   })
 
 }
@@ -259,10 +277,10 @@ const cate_2 = 'limit=1&page=1'
 async function nameCate_2() {
   const cates_list = await getCateList(cate_2);
   const getNameFoods = document.querySelector(".organic-list-id");
-  await cates_list .data.map((cate, index) => {
-      const getNames = document.createElement("div");
-      getNames.className = "img-organic";
-      getNames.innerHTML = `
+  await cates_list.data.map((cate, index) => {
+    const getNames = document.createElement("div");
+    getNames.className = "img-organic";
+    getNames.innerHTML = `
       <img src="${cate.cateImage}" alt="">
       <div class="organic-button">
            <div class="name-organic">
@@ -287,7 +305,7 @@ async function nameCate_2() {
            </div>
       </div>
       `;
-      getNameFoods.appendChild(getNames)
+    getNameFoods.appendChild(getNames)
   })
 
 }
@@ -296,12 +314,12 @@ nameCate_2();
 
 
 // list-menu-cate
-const  limit = 'limit=6&page=1'
+const limit = 'limit=6&page=1'
 async function listCateFood() {
   const foods = await getListFood(limit);
   const getFoodList = document.querySelector(".navba-menu-list");  // lấy thẻ class chứa tát các thẻ
   await foods.data.map((food, index) => {  // chuyền food vào
-      const getNameList = document.createElement("div"); // thấy thẻ div
+      const getNameList = document.createElement("div"); // tạo thẻ div
       getNameList.className = "navba-menu"; // lấy thẻ cha các thẻ
       getNameList.innerHTML = `
       <a href="#" class="navba-list-menu">
@@ -318,7 +336,6 @@ async function listCateFood() {
               <i class="fa-regular fa-star"></i>
               <i class="fa-regular fa-star"></i>
               <i class="fa-regular fa-star"></i> 
-
            </div>
            <div class="list-money">
               <span>${food.price}</span>
@@ -326,14 +343,230 @@ async function listCateFood() {
       </div>
   </a>
       `;
-      getFoodList.appendChild(getNameList)
+    getFoodList.appendChild(getNameList)
   })
 
 }
 listCateFood();
 
+//call api add to cart
+async function addFoodToCart(event) {
+  const btn = event.target
+  const icons = btn.parentElement
+  const menu = icons.parentElement
+  const buyAmount = menu.querySelector("#buy-amount")
+  const amountInput = buyAmount.querySelector("input")
+  const inputValue = parseInt(amountInput.value)
+  const foodId = event.target.id;
+  const data = {
+    foodId: foodId,
+    quantity: inputValue
+  }
+  amountInput.value = 1 
+  await useAddToCart(data);
+  await getQuatityProduct()
+}
+
+//api get list order
+function listOrder(event) {
+  const foodId = event.target.id;
+  const data = {
+    foodId: foodId,
+    quantity: 1
+  }
+  useAddToCart(data);
+}
+
+async function useAddToCart(data) {
+  let token = localStorage.getItem("food-token");
+  try {
+    await axios({
+      method: 'post',
+      url: 'https://food-api.huytx.com/api/v1/order/add-to-cart',
+      data: data,
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    let objOfToast = {
+      title: "success",
+      message: "Đã thêm sản phẩm",
+      duration: 3000
+    }
+    toastMessage(objOfToast)
+    listShoppingCart();
+  } catch (error) {
+    let objOfToast = {
+      title: "error",
+      message: error,
+      duration: 3000
+    }
+    toastMessage(objOfToast)
+  }
+}
+
+async function getShoppingCart() {
+  let token = localStorage.getItem("food-token");
+  try {
+    const data = await axios({
+      method: 'get',
+      url: 'https://food-api.huytx.com/api/v1/order/shopping-cart',
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    return data.data;
+  } catch (e) {
+    console.log(e)
+  }
+}
 
 
+function checkCartProduct() {
+  document.querySelector('.cart').style.right = "0";
+  const list = document.querySelector(".list");
+  const noContent = document.querySelector(".noContent");
+  const cartNotice = document.querySelector(".cart-notice");
 
 
+  if (cartNotice.innerHTML === "0") {
+      list.style.display = "none";
+      noContent.style.display = "block";
+  } else {
+      listShoppingCart();
+      list.style.display = "block";
+      noContent.style.display = "none";
+  }
+}
 
+async function listShoppingCart() {
+  const shoppings = await getShoppingCart();
+  const listShop = document.querySelector('tbody');
+  listShop.innerHTML='';
+  shoppings.data.items.map((food, index) => {
+    const shopFood = document.createElement('tr');
+    shopFood.innerHTML = `
+        <td style="display: flex; align-items: center">
+        <img
+          width="70px"
+          style="margin-right: 5px"
+          src="${food.images[0].imageUrl}"
+          alt="">${food.foodName}
+      </td>
+      <td style="text-align: center">
+        <p>
+        <span>${food.price}</span>
+        <sup>đ</sup>
+        </p>
+      </td>
+      <td style="cursor: pointer">
+      <span class="cart-delete">Xóa</span>
+      </td>;
+      `
+    listShop.append(shopFood)
+  });
+}
+
+//toast message
+function toastMessage({ title, message, duration }) {
+  const main = document.getElementById('toast');
+  const toast = document.createElement("div");
+
+  const icons = {
+    Success: 'fa-sharp fa-regular fa-circle-check',
+    Error: 'fas fa-exclamation-circle'
+  }
+  toast.classList.add('toast', `toast--${title}`)
+  // toast.classList.add('toast',`toast--${title}`)
+  toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${(duration / 1000).toFixed(2)}s forwards`;
+  toast.innerHTML = `
+  <div class="toast__icon">
+      <i class="${icons[title]}"></i>
+  </div>
+  <div class="toast__body">
+      <h3 class="toast__title">${title}</h3>
+      <p class="toast__msg">
+          ${message}
+      </p>
+  </div>
+  <div class="toast__close">
+      <i class="fa-solid fa-xmark"></i>
+  </div>`
+  main.appendChild(toast);
+  setTimeout(function () {
+    main.removeChild(toast)
+  }, (duration + 1000))
+
+  toast.onclick = function (e) {
+    console.log(e.target);
+    if (e.target.closest('.toast__close')) {
+      main.removeChild(toast)
+    }
+    console.log(e.target.closest('toast__close'));
+    clearTimeout(autoRemoveId)
+  }
+}
+
+//show quatity product to cart
+getQuatityProduct()
+async function getQuatityProduct() {
+  let token = localStorage.getItem("food-token");
+  try {
+    const { data } = await axios({
+      method: 'get',
+      url: 'https://food-api.huytx.com/api/v1/order/count/shopping-cart',
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    //display quatity product to cart
+    const cartNotice = document.querySelector(".cart-notice")
+    if (data.data.total < 100) {
+      cartNotice.textContent = data.data.total
+    } else {
+      cartNotice.textContent = "99+"
+    }
+
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+
+// list order 
+
+async function listOrderMenu() {
+  const shiping = await getShoppingCart();
+  const listOrderProduct = document.querySelector('.box-order');
+  shiping.data.items.map((food) => {
+    const listProduct = document.createElement('div');
+    listProduct.className = "shipping"
+    listProduct.innerHTML = `
+    <div class="cart-car">
+    <i class="fa-solid fa-truck-fast"></i>
+    <span>Đơn hàng đang xuất kho</span>
+  </div>
+
+  <div class="information-order">
+      <div class="img-and-name">
+          <img src="${food.images[0].imageUrl}" alt="">
+          <span>${food.foodName}</span>
+          <span>Số lượng :${food.quantity}</span>
+      </div>
+
+      <div class="price">${food.price}Đ</div>
+      
+  </div>
+
+  <div class="count-btn">
+    <span class="color-count">Tổng tiền: <span>${food.price}</span>đ</span>
+    <div class="two-btn">
+      <a href="#"><button>Mua lại</button></a>
+      <a href="#"><button>Chi tiết</button></a>
+    </div>
+  </div>
+      `
+      listOrderProduct.append(listProduct)
+  });
+}
+listOrderMenu()
--- modules/profile/js/profile.js
+++ modules/profile/js/profile.js
@@ -1,25 +1,51 @@
+const any = document.querySelector.bind(document);
+const many = document.querySelectorAll.bind(document);
+
+const as = many(".information");
+const bs = many(".title-information");
+
+as.forEach((a ,index) => {
+  const b = bs[index];
+
+  a.onclick = function () {
+
+    any(".information.active-profile").classList.remove("active-profile");
+    any(".title-information.open").classList.remove("open");
+
+    this.classList.add("active-profile");
+    b.classList.add("open");
+
+  };
+});
+
+
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-var tabs = $$(".list-info__item");
+const tabs = $$(".tab-item");
+const panes = $$(".tab-pane");
 
-tabs.forEach((tab) =>{
-    tab.onclick = () => {
-        const active = $(".list-info__item.active-profile")
-        active.classList.remove("active-profile")
-        tab.classList.add("active-profile")
-    }
-})
+const tabActive = $(".tab-item.active");
+const line = $(".tabs .line");
 
-var cover = document.querySelector(".cover")
-var theList = document.getElementById("list")
-var list = window.getComputedStyle(theList)
-function myFunction() {
-    if (list.getPropertyValue("display") === "none") {
-        theList.style.display = "block";
-        cover.style.display = "block"
-    } else {
-        theList.style.display = "none"
-        cover.style.display = "none"
-    }
-}
+// SonDN fixed - Active size wrong size on first load.
+// Original post: https://www.facebook.com/groups/649972919142215/?multi_permalinks=1175881616551340
+requestIdleCallback(function () {
+  line.style.left = tabActive.offsetLeft + "px";
+  line.style.width = tabActive.offsetWidth + "px";
+});
+
+tabs.forEach((tab, index) => {
+  const pane = panes[index];
+
+  tab.onclick = function () {
+    $(".tab-item.active").classList.remove("active");
+    $(".tab-pane.active").classList.remove("active");
+
+    line.style.left = this.offsetLeft + "px";
+    line.style.width = this.offsetWidth + "px";
+
+    this.classList.add("active");
+    pane.classList.add("active");
+  };
+});
